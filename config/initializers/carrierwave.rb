a1a77268634c7e7f2f474f7cd185bdeead304364
tatsuya fukui <nakedtatsuya6@gmail.com>
2019-02-05 17:37:36 +0900

    env

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV["AWS_ACCESS"],
-      aws_secret_access_key: ENV["AWS_SECRET"],
+      aws_access_key_id: ENV["AWS_ACCESS_KEY"],
+      aws_secret_access_key: ENV["AWS_SECRET_KEY"],
       region: 'ap-northeast-1'
   }
 
